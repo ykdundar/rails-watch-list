4cd11a0204e5dc8084c7e91170b97215a2c8f3fa
Yesim Dundar <kuranyesim@gmail.com>
2023-02-23 23:04:53 +0100

    complete backend

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,29 +1,37 @@
 class BookmarksController < ApplicationController
-  before_action :set_list
+  before_action :set_list, only: [:new, :create]
+  before_action :set_bookmark, only: :destroy
 
   def new
-    @bookmark = @list.bookmarks.new
+    @bookmark = Bookmark.new
   end
 
   def create
-    @list.bookmarks.new(bookmark_params)
-
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
     if @bookmark.save
-      redirect_to root_path
+      redirect_to list_path(@list)
     else
-      puts @bookmark.errors.first
-
-      render :new
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
 
-  def set_list
-    @list = List.find(params[:list_id])
-  end
-
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,12 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: :show
+  before_action :set_list, only: [:show, :destroy]
 
   def index
     @lists = List.all
   end
 
   def show
+
   end
 
   def new
@@ -16,10 +17,15 @@ class ListsController < ApplicationController
     @list = List.new(list_param)
 
     if @list.save
-      redirect_to root_path
+      redirect_to list_path(@list)
     else
-      redirect_to root_path
+      render :new, status: :unprocessable_entity
     end
+  end
+  
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
   end
 
   private
